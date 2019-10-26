1eb69f385182f1ba25c78abbaca6fb4ed298a969
rohitm53 <36997587+rohitm53@users.noreply.github.com>
2019-10-27 02:12:58 +0530

    project created

---
+++ src/main/java/com/indiacleantool/cleantool/CleanToolApplication.java
@@ -0,0 +1,13 @@
+package com.indiacleantool.cleantool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanToolApplication.class,args);
+        System.out.println("Nirmal Babaji");
+    }
+}
